7627d030414248b77fd7dcedc4253b78f3bdeac0
Ruben Allenspach <rallenspach97@gmail.com>
2019-05-15 21:01:05 +0200

    file restructuring

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 from pydub import AudioSegment
+import os
 
 
-song = AudioSegment.from_mp3("1jo01/1jo01g.mp3")
-
-cuts = [
-    9400,
-    20740,
-    31000,
-    44700,
-    49000,
-    59500,
-    68400,
-    79000,
-    86200,
-    96400,
-    1037000
-]
+audio_cuts = {
+    "1jo01g.mp3": [
+        9400,
+        20740,
+        31000,
+        44700,
+        49000,
+        59500,
+        68400,
+        79000,
+        86200,
+        96400,
+        1037000
+    ]
+}
 
 last_cut = 0
 counter = 0
 
-for cut in cuts:
-    verse = song[last_cut:cut]
-    verse.export("verse_" + str(counter) + ".mp3", format="mp3")
+for audio, cuts in audio_cuts.items():
+    song = AudioSegment.from_mp3("audio/" + audio)
+    path = "cuts/" + audio
 
-    counter = counter + 1
-    last_cut = cut
+    os.mkdir(path)
+
+    for cut in cuts:
+        verse = song[last_cut:cut]
+        verse.export(path + "/verse_" + str(counter) + ".mp3", format="mp3")
+
+        counter = counter + 1
+        last_cut = cut
