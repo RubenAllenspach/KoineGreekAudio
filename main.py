f42b9f251f473c41a5e88dcea127bf9f864bf34a
Ruben Allenspach <rallenspach97@gmail.com>
2019-05-16 20:56:38 +0200

    trying to create an interface for cutting audio

--- main.py
+++ main.py
@@ -2,19 +2,37 @@ from pydub import AudioSegment
 import os
 
 
+"""
+"1jo01g.mp3": [
+    9400,
+    20740,
+    31000,
+    44700,
+    49000,
+    59500,
+    68400,
+    79000,
+    86200,
+    96400,
+    1037000
+],
+"""
+
 audio_cuts = {
-    "1jo01g.mp3": [
-        9400,
-        20740,
-        31000,
-        44700,
-        49000,
-        59500,
-        68400,
-        79000,
-        86200,
-        96400,
-        1037000
+    "1jo02g.mp3": [
+        9970,
+        22360,
+        30764,
+        37127,
+        45797,
+        55557,
+        62157,
+        73815,
+        84708,
+        92648,
+        99065,
+        110756,
+        117226
     ]
 }
 
@@ -25,10 +43,11 @@ for audio, cuts in audio_cuts.items():
     song = AudioSegment.from_mp3("audio/" + audio)
     path = "cuts/" + audio
 
-    os.mkdir(path)
+    if not os.path.exists(path):
+        os.mkdir(path)
 
     for cut in cuts:
-        verse = song[last_cut:cut]
+        verse = song[int(last_cut):int(cut)]
         verse.export(path + "/verse_" + str(counter) + ".mp3", format="mp3")
 
         counter = counter + 1
